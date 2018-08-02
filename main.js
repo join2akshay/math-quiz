5b69e688c0c31c12c34f5a470cad3d5d2174f555
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2018-08-02 23:12:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-
+let playing=false;
+let score=0;
+let action;
+let timeremain;
+document.getElementById('start').onclick=function(){
+    
+    if(playing===true)
+        {
+           location.reload(); 
+        }
+    else
+    {
+        playing=true;
+        document.getElementById('scoreValue').innerHTML = score ;
+        document.getElementById('time').style.display="block"; 
+        timeremain=60;
+        
+        document.getElementById('start').innerHTML="Reset Game";
+       
+        startCount();
+    }
+    
+    
+    
+}
